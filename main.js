39923d61cf02d4c2681da77dcf0b4a6c130ebf40
Ayo <1835516740@qq.com>
2024-06-24 12:04:00 +0800

    更新站点

--- main.js
+++ main.js
@@ -11,12 +11,22 @@ window.onload = () => {
 
     /////////////////////////////////////////////////////////
 
-    $(`#mathGraphics`).click(() => {
-        window.open(`mathGraphics/index.html`);
+    $(`#mathGraphics0`).click(() => {
+        //[腾讯云， cloudflare]
+        const urlList = [`https://math-gfx-1327442820.cos.ap-guangzhou.myqcloud.com/web-mobile/index.html`, `https://math-gfx.pages.dev/`];
+        const url = urlList[0];
+        let res = window.open(url, `_blank`, `width=1920,height=1280`);
+    })
+
+    $(`#mathGraphics1`).click(() => {
+        //[腾讯云， cloudflare]
+        const urlList = [`https://math-gfx-1327442820.cos.ap-guangzhou.myqcloud.com/web-mobile/index.html`, `https://math-gfx.pages.dev/`];
+        const url = urlList[1];
+        let res = window.open(url, `_blank`, `width=1920,height=1280`);
     })
 
     $(`#encryption`).click(() => {
-        window.open(`encryption/index.html`);
+        window.open(`encryption/index.html`, `_self`);
     })
 
 }
